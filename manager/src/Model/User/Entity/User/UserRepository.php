146d8363c89b35ad902ea6e5029a0a8029ab4880
olan <bee.olan@mail.ru>
2022-01-04 16:51:58 +0300

    0d8a034 1156 - строка

--- manager/src/Model/User/Entity/User/UserRepository.php
+++ manager/src/Model/User/Entity/User/UserRepository.php
@@ -6,6 +6,7 @@ namespace App\Model\User\Entity\User;
 
 interface UserRepository
 {
+	public function findByConfirmToken(string $token): ?User;
     public function hasByEmail(Email $email): bool;
 
     public function add(User $user): void;
