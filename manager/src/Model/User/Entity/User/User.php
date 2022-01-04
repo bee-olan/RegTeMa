185a13577f8e7848c35c8ba3e5efebb49171a686
olan <bee.olan@mail.ru>
2022-01-04 12:19:32 +0300

    Начало User + Test

---
+++ manager/src/Model/User/Entity/User/User.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Entity\User;
+
+class User
+{
+	/**
+	 * @var string
+	 */
+	private $email;
+	/**
+	 * @var string
+	 */
+	private $passwordHash;
+
+	public function __construct(string $email, string $hash)
+	{
+		$this->email = $email;
+		$this->passwordHash = $hash;
+	}
+
+	public function getEmail(): string
+	{
+		return $this->email;
+	}
+
+	public function getPasswordHash(): string
+	{
+		return $this->passwordHash;
+	}
+}
+
