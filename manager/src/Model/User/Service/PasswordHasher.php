94f65dc72f886ff3eef3ac1345b51ff53201b8ed
olan <bee.olan@mail.ru>
2022-01-06 20:36:00 +0300

    Реализована проверка учетных данных

--- manager/src/Model/User/Service/PasswordHasher.php
+++ manager/src/Model/User/Service/PasswordHasher.php
@@ -14,4 +14,10 @@ class PasswordHasher
         }
         return $hash;
     }
+
+	public function validate(string $password, string $hash): bool
+	{
+		return password_verify($password, $hash);
+	}
 }
+
