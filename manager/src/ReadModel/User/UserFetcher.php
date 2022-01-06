2f4de1a93fab3f0411a50ce76944f15d30d0187e
olan <bee.olan@mail.ru>
2022-01-06 20:30:48 +0300

    Added custom user provider

--- manager/src/ReadModel/User/UserFetcher.php
+++ manager/src/ReadModel/User/UserFetcher.php
@@ -5,23 +5,44 @@ declare(strict_types=1);
 namespace App\ReadModel\User;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\FetchMode;
 
 class UserFetcher
 {
-    private $connection;
+	private $connection;
 
-    public function __construct(Connection $connection)
-    {
-        $this->connection = $connection;
-    }
+	public function __construct(Connection $connection)
+	{
+		$this->connection = $connection;
+	}
 
-    public function existsByResetToken(string $token): bool
-    {
-        return $this->connection->createQueryBuilder()
-            ->select('COUNT (*)')
-            ->from('user_users')
-            ->where('reset_token_token = :token')
-            ->setParameter(':token', $token)
-            ->execute()->fetchColumn(0) > 0;
-    }
+	public function existsByResetToken(string $token): bool
+	{
+		return $this->connection->createQueryBuilder()
+				->select('COUNT (*)')
+				->from('user_users')
+				->where('reset_token_token = :token')
+				->setParameter(':token', $token)
+				->execute()->fetchColumn(0) > 0;
+	}
+
+	public function findForAuth(string $email): ?AuthView
+	{
+		$stmt = $this->connection->createQueryBuilder()
+			->select(
+				'id',
+				'email',
+				'password_hash',
+				'role'
+			)
+			->from('user_users')
+			->where('email = :email')
+			->setParameter(':email', $email)
+			->execute();
+
+		$stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, AuthView::class);
+		$result = $stmt->fetch();
+
+		return $result ?: null;
+	}
 }
