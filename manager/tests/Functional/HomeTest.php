091adc5eb751429252ec0f073a0161c0ef19998d
olan <bee.olan@mail.ru>
2022-01-12 08:16:04 +0300

    Удален  привет

--- manager/tests/Functional/HomeTest.php
+++ manager/tests/Functional/HomeTest.php
@@ -8,24 +8,24 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeTest extends WebTestCase
 {
-    public function testGuest(): void
-    {
-        $client = static::createClient();
-        $client->request('GET', '/');
+	public function testGuest(): void
+	{
+		$client = static::createClient();
+		$client->request('GET', '/');
 
-        $this->assertSame(302, $client->getResponse()->getStatusCode());
-        $this->assertSame('http://localhost/login', $client->getResponse()->headers->get('Location'));
-    }
+		$this->assertSame(302, $client->getResponse()->getStatusCode());
+		$this->assertSame('http://localhost/login', $client->getResponse()->headers->get('Location'));
+	}
 
-    public function testSuccess(): void
-    {
-        $client = static::createClient([], [
-            'PHP_AUTH_USER' => 'admin@app.test',
-            'PHP_AUTH_PW' => 'password',
-        ]);
-        $crawler = $client->request('GET', '/');
+	public function testSuccess(): void
+	{
+		$client = static::createClient([], [
+			'PHP_AUTH_USER' => 'admin@app.test',
+			'PHP_AUTH_PW' => 'password',
+		]);
+		$crawler = $client->request('GET', '/');
 
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Hello', $crawler->filter('h1')->text());
-    }
+		$this->assertSame(200, $client->getResponse()->getStatusCode());
+		$this->assertContains('Home', $crawler->filter('title')->text());
+	}
 }
