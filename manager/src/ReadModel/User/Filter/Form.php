9f701b9a222e96b9ea63969f78fc553137fb5f2c
olan <bee.olan@mail.ru>
2022-01-13 20:50:46 +0300

    Добавлена автоматическая отправка формы фильтра

--- manager/src/ReadModel/User/Filter/Form.php
+++ manager/src/ReadModel/User/Filter/Form.php
@@ -14,18 +14,24 @@ class Form extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', Type\TextType::class, ['required' => false, 'attr' => ['placeholder' => 'Ф.И. или Ник']])
-            ->add('email', Type\TextType::class, ['required' => false, 'attr' => ['placeholder' => 'Эл.почта']])
+            ->add('name', Type\TextType::class, ['required' => false, 'attr' => ['placeholder' => 'Ф.И. или Ник',
+							'onchange' => 'this.form.submit()',
+						]])
+            ->add('email', Type\TextType::class, ['required' => false, 'attr' => ['placeholder' => 'Эл.почта',
+							'onchange' => 'this.form.submit()',
+						]])
             ->add('status', Type\ChoiceType::class, ['choices' => [
                 'Ожидает' => User::STATUS_WAIT,
                 'Активен' => User::STATUS_ACTIVE,
                 'Блокирован' => User::STATUS_BLOCKED,
-            ], 'required' => false, 'placeholder' => 'Все статусы'])
+            ], 'required' => false, 'placeholder' => 'Все статусы',
+								'attr' => ['onchange' => 'this.form.submit()']])
             ->add('role', Type\ChoiceType::class, ['choices' => [
                 'Пользователи' => Role::USER,
 								'Модераторы' => Role::MODERATOR,
                 'Админы' => Role::ADMIN,
-            ], 'required' => false, 'placeholder' => 'Все роли']);
+            ], 'required' => false, 'placeholder' => 'Все роли',
+								'attr' => ['onchange' => 'this.form.submit()']]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
