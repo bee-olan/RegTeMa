7f2a3a9f9dcd432d2d1e682fff4c997e09ab7fa7
olan <bee.olan@mail.ru>
2022-01-25 10:12:41 +0300

    Улучшенное приспособление для пользователя ---------  фикстуры

--- manager/src/DataFixtures/UserFixture.php
+++ manager/src/DataFixtures/UserFixture.php
@@ -24,21 +24,72 @@ class UserFixture extends Fixture
 	{
 		$hash = $this->hasher->hash('password');
 
-		$user = User::signUpByEmail(
-			Id::next(),
-			new \DateTimeImmutable(),
-			new Name('Olan', 'Bee'),
-			new Email('admin@app.test'),
-			$hash,
-			'token'
+		$network = $this->createSignedUpByNetwork(
+			new Name('David', 'Black'),
+			'facebook',
+			'1000000'
 		);
+		$manager->persist($network);
 
-		$user->confirmSignUp();
+		$requested = $this->createSignUpRequestedByEmail(
+			new Name('John', 'Doe'),
+			new Email('requested@app.test'),
+			$hash
+		);
+		$manager->persist($requested);
 
-		$user->changeRole(Role::admin());
+		$confirmed = $this->createSignUpConfirmedByEmail(
+			new Name('Brad', 'Pitt'),
+			new Email('user@app.test'),
+			$hash
+		);
+		$manager->persist($confirmed);
 
-		$manager->persist($user);
+		$admin = $this->createAdminByEmail(
+			new Name('James', 'Bond'),
+			new Email('admin@app.test'),
+			$hash
+		);
+		$manager->persist($admin);
 
 		$manager->flush();
 	}
+
+	private function createAdminByEmail(Name $name, Email $email, string $hash): User
+	{
+		$user = $this->createSignUpConfirmedByEmail($name, $email, $hash);
+		$user->changeRole(Role::admin());
+		return $user;
+	}
+
+	private function createSignUpConfirmedByEmail(Name $name, Email $email, string $hash): User
+	{
+		$user = $this->createSignUpRequestedByEmail($name, $email, $hash);
+		$user->confirmSignUp();
+		return $user;
+	}
+
+	private function createSignUpRequestedByEmail(Name $name, Email $email, string $hash): User
+	{
+		return User::signUpByEmail(
+			Id::next(),
+			new \DateTimeImmutable(),
+			$name,
+			$email,
+			$hash,
+			'token'
+		);
+	}
+
+	private function createSignedUpByNetwork(Name $name, string $network, string $identity): User
+	{
+		return User::signUpByNetwork(
+			Id::next(),
+			new \DateTimeImmutable(),
+			$name,
+			$network,
+			$identity
+		);
+	}
 }
+
