41d967c5530a2d1b74890b36e4bf49027675f3e9
olan <bee.olan@mail.ru>
2022-01-29 13:59:44 +0300

    Added project members fixture

--- manager/src/DataFixtures/Work/Projects/ProjectFixture.php
+++ manager/src/DataFixtures/Work/Projects/ProjectFixture.php
@@ -4,44 +4,62 @@ declare(strict_types=1);
 
 namespace App\DataFixtures\Work\Projects;
 
+use App\DataFixtures\Work\Members\MemberFixture;
+use App\Model\User\Entity\User\Role;
+use App\Model\Work\Entity\Members\Member\Member;
 use App\Model\Work\Entity\Projects\Project\Department\Id as DepartmentId;
 use App\Model\Work\Entity\Projects\Project\Project;
 use App\Model\Work\Entity\Projects\Project\Id;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ProjectFixture extends Fixture
+class ProjectFixture extends Fixture implements DependentFixtureInterface
 {
-	public function load(ObjectManager $manager): void
-	{
-		$active = $this->createProject('Первый Project', 1);
-		$active->addDepartment(DepartmentId::next(), 'Итальянки');
-		$active->addDepartment(DepartmentId::next(), 'Бак-фаст');
-		$manager->persist($active);
+    public function load(ObjectManager $manager): void
+    {
+        /** @var Member $admin */
+        $admin = $this->getReference(MemberFixture::REFERENCE_ADMIN);
 
-		$active = $this->createProject('Second Project', 2);
-		$manager->persist($active);
+        /** @var Role $manage */
+        $manage = $this->getReference(RoleFixture::REFERENCE_MANAGER);
 
-		$archived = $this->createArchivedProject('Third Project', 3);
-		$manager->persist($archived);
+        $active = $this->createProject('First Project', 1);
+        $active->addDepartment($development = DepartmentId::next(), 'Development');
+        $active->addDepartment(DepartmentId::next(), 'Marketing');
+        $active->addMember($admin, [$development], [$manage]);
+        $manager->persist($active);
 
-		$manager->flush();
-	}
+        $active = $this->createProject('Second Project', 2);
+        $manager->persist($active);
 
-	private function createArchivedProject(string $name, int $sort): Project
-	{
-		$project = $this->createProject($name, $sort);
-		$project->archive();
-		return $project;
-	}
+        $archived = $this->createArchivedProject('Third Project', 3);
+        $manager->persist($archived);
 
-	private function createProject(string $name, int $sort): Project
-	{
-		return new Project(
-			Id::next(),
-			$name,
-			$sort
-		);
-	}
+        $manager->flush();
+    }
+
+    private function createArchivedProject(string $name, int $sort): Project
+    {
+        $project = $this->createProject($name, $sort);
+        $project->archive();
+        return $project;
+    }
+
+    private function createProject(string $name, int $sort): Project
+    {
+        return new Project(
+            Id::next(),
+            $name,
+            $sort
+        );
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            MemberFixture::class,
+            RoleFixture::class,
+        ];
+    }
 }
-
