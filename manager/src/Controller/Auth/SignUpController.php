adc53224c7aa3b3f7579fce2f38bbbe6ee333323
olan <bee.olan@mail.ru>
2022-01-30 11:16:25 +0300

    Добавлен обработчик ошибок контроллера 659 10  мин

--- manager/src/Controller/Auth/SignUpController.php
+++ manager/src/Controller/Auth/SignUpController.php
@@ -7,7 +7,7 @@ namespace App\Controller\Auth;
 use App\Model\User\UseCase\SignUp;
 use App\ReadModel\User\UserFetcher;
 use App\Security\LoginFormAuthenticator;
-use Psr\Log\LoggerInterface;
+use App\Controller\ErrorHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,82 +17,82 @@ use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class SignUpController extends AbstractController
 {
-	private $users;
-	private $logger;
+    private $users;
+    private $errors;
 
-	public function __construct(UserFetcher $users, LoggerInterface $logger)
-	{
-		$this->users = $users;
-		$this->logger = $logger;
-	}
+    public function __construct(UserFetcher $users, ErrorHandler $errors)
+    {
+        $this->users = $users;
+        $this->errors = $errors;
+    }
 
-	/**
-	 * @Route("/signup", name="auth.signup")
-	 * @param Request $request
-	 * @param SignUp\Request\Handler $handler
-	 * @return Response
-	 */
-	public function request(Request $request, SignUp\Request\Handler $handler): Response
-	{
-		$command = new SignUp\Request\Command();
+    /**
+     * @Route("/signup", name="auth.signup")
+     * @param Request $request
+     * @param SignUp\Request\Handler $handler
+     * @return Response
+     */
+    public function request(Request $request, SignUp\Request\Handler $handler): Response
+    {
+        $command = new SignUp\Request\Command();
 
-		$form = $this->createForm(SignUp\Request\Form::class, $command);
-		$form->handleRequest($request);
+        $form = $this->createForm(SignUp\Request\Form::class, $command);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid()) {
-			try {
-				$handler->handle($command);
-				$this->addFlash('success', 'Check your email.');
-				return $this->redirectToRoute('home');
-			} catch (\DomainException $e) {
-				$this->logger->warning($e->getMessage(), ['exception' => $e]);
-				$this->addFlash('error', $e->getMessage());
-			}
-		}
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                $this->addFlash('success', 'Check your email.');
+                return $this->redirectToRoute('home');
+            } catch (\DomainException $e) {
+                $this->errors->handle($e);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
 
-		return $this->render('app/auth/signup.html.twig', [
-			'form' => $form->createView(),
-		]);
-	}
+        return $this->render('app/auth/signup.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
-	/**
-	 * @Route("/signup/{token}", name="auth.signup.confirm")
-	 * @param Request $request
-	 * @param string $token
-	 * @param SignUp\Confirm\ByToken\Handler $handler
-	 * @param UserProviderInterface $userProvider
-	 * @param GuardAuthenticatorHandler $guardHandler
-	 * @param LoginFormAuthenticator $authenticator
-	 * @return Response
-	 */
-	public function confirm(
-		Request $request,
-		string $token,
-		SignUp\Confirm\ByToken\Handler $handler,
-		UserProviderInterface $userProvider,
-		GuardAuthenticatorHandler $guardHandler,
-		LoginFormAuthenticator $authenticator
-	): Response
-	{
-		if (!$user = $this->users->findBySignUpConfirmToken($token)) {
-			$this->addFlash('error', 'Incorrect or already confirmed token.');
-			return $this->redirectToRoute('auth.signup');
-		}
+    /**
+     * @Route("/signup/{token}", name="auth.signup.confirm")
+     * @param Request $request
+     * @param string $token
+     * @param SignUp\Confirm\ByToken\Handler $handler
+     * @param UserProviderInterface $userProvider
+     * @param GuardAuthenticatorHandler $guardHandler
+     * @param LoginFormAuthenticator $authenticator
+     * @return Response
+     */
+    public function confirm(
+        Request $request,
+        string $token,
+        SignUp\Confirm\ByToken\Handler $handler,
+        UserProviderInterface $userProvider,
+        GuardAuthenticatorHandler $guardHandler,
+        LoginFormAuthenticator $authenticator
+    ): Response
+    {
+        if (!$user = $this->users->findBySignUpConfirmToken($token)) {
+            $this->addFlash('error', 'Incorrect or already confirmed token.');
+            return $this->redirectToRoute('auth.signup');
+        }
 
-		$command = new SignUp\Confirm\ByToken\Command($token);
+        $command = new SignUp\Confirm\ByToken\Command($token);
 
-		try {
-			$handler->handle($command);
-			return $guardHandler->authenticateUserAndHandleSuccess(
-				$userProvider->loadUserByUsername($user->email),
-				$request,
-				$authenticator,
-				'main'
-			);
-		} catch (\DomainException $e) {
-			$this->logger->warning($e->getMessage(), ['exception' => $e]);
-			$this->addFlash('error', $e->getMessage());
-			return $this->redirectToRoute('auth.signup');
-		}
-	}
+        try {
+            $handler->handle($command);
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $userProvider->loadUserByUsername($user->email),
+                $request,
+                $authenticator,
+                'main'
+            );
+        } catch (\DomainException $e) {
+            $this->errors->handle($e);
+            $this->addFlash('error', $e->getMessage());
+            return $this->redirectToRoute('auth.signup');
+        }
+    }
 }
--- manager/src/Controller/UsersController.php
+++ manager/src/Controller/UsersController.php
@@ -14,7 +14,7 @@ use App\Model\User\UseCase\SignUp\Confirm;
 use App\ReadModel\User\Filter;
 use App\ReadModel\User\UserFetcher;
 use App\ReadModel\Work\Members\Member\MemberFetcher;
-use Psr\Log\LoggerInterface;
+use App\Controller\ErrorHandler;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,232 +27,230 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class UsersController extends AbstractController
 {
-	private const PER_PAGE = 2;
+    private const PER_PAGE = 10;
 
-	private $logger;
+    private $errors;
 
-	public function __construct(LoggerInterface $logger)
-	{
-		$this->logger = $logger;
-	}
+    public function __construct(ErrorHandler $errors)
+    {
+        $this->errors = $errors;
+    }
 
-	/**
-	 * @Route("", name="")
-	 * @param Request $request
-	 * @param UserFetcher $fetcher
-	 * @return Response
-	 */
-	public function index(Request $request, UserFetcher $fetcher): Response
-	{
-		$filter = new Filter\Filter();
+    /**
+     * @Route("", name="")
+     * @param Request $request
+     * @param UserFetcher $fetcher
+     * @return Response
+     */
+    public function index(Request $request, UserFetcher $fetcher): Response
+    {
+        $filter = new Filter\Filter();
 
-		$form = $this->createForm(Filter\Form::class, $filter);
-		$form->handleRequest($request);
+        $form = $this->createForm(Filter\Form::class, $filter);
+        $form->handleRequest($request);
 
-		$pagination = $fetcher->all(
-			$filter,
-			$request->query->getInt('page', 1),
-			self::PER_PAGE,
-			$request->query->get('sort', 'date'),
-			$request->query->get('direction', 'desc')
-		);
+        $pagination = $fetcher->all(
+            $filter,
+            $request->query->getInt('page', 1),
+            self::PER_PAGE,
+            $request->query->get('sort', 'date'),
+            $request->query->get('direction', 'desc')
+        );
 
-		return $this->render('app/users/index.html.twig', [
-			'pagination' => $pagination,
-			'form' => $form->createView(),
-		]);
-	}
+        return $this->render('app/users/index.html.twig', [
+            'pagination' => $pagination,
+            'form' => $form->createView(),
+        ]);
+    }
 
-	/**
-	 * @Route("/create", name=".create")
-	 * @param Request $request
-	 * @param Create\Handler $handler
-	 * @return Response
-	 */
-	public function create(Request $request, Create\Handler $handler): Response
-	{
-		$command = new Create\Command();
+    /**
+     * @Route("/create", name=".create")
+     * @param Request $request
+     * @param Create\Handler $handler
+     * @return Response
+     */
+    public function create(Request $request, Create\Handler $handler): Response
+    {
+        $command = new Create\Command();
 
-		$form = $this->createForm(Create\Form::class, $command);
-		$form->handleRequest($request);
+        $form = $this->createForm(Create\Form::class, $command);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid()) {
-			try {
-				$handler->handle($command);
-				return $this->redirectToRoute('users');
-			} catch (\DomainException $e) {
-				$this->logger->warning($e->getMessage(), ['exception' => $e]);
-				$this->addFlash('error', $e->getMessage());
-			}
-		}
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                return $this->redirectToRoute('users');
+            } catch (\DomainException $e) {
+                $this->errors->handle($e);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
 
-		return $this->render('app/users/create.html.twig', [
-			'form' => $form->createView(),
-		]);
-	}
+        return $this->render('app/users/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
-	/**
-	 * @Route("/{id}/edit", name=".edit")
-	 * @param User $user
-	 * @param Request $request
-	 * @param Edit\Handler $handler
-	 * @return Response
-	 */
-	public function edit(User $user, Request $request, Edit\Handler $handler): Response
-	{
-		if ($user->getId()->getValue() === $this->getUser()->getId()) {
-			$this->addFlash('error', 'Невозможно отредактировать себя.');
-			return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-		}
+    /**
+     * @Route("/{id}/edit", name=".edit")
+     * @param User $user
+     * @param Request $request
+     * @param Edit\Handler $handler
+     * @return Response
+     */
+    public function edit(User $user, Request $request, Edit\Handler $handler): Response
+    {
+        if ($user->getId()->getValue() === $this->getUser()->getId()) {
+            $this->addFlash('error', 'Unable to edit yourself.');
+            return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+        }
 
-		$command = Edit\Command::fromUser($user);
+        $command = Edit\Command::fromUser($user);
 
-		$form = $this->createForm(Edit\Form::class, $command);
-		$form->handleRequest($request);
+        $form = $this->createForm(Edit\Form::class, $command);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid()) {
-			try {
-				$handler->handle($command);
-				return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-			} catch (\DomainException $e) {
-				$this->logger->warning($e->getMessage(), ['exception' => $e]);
-				$this->addFlash('error', $e->getMessage());
-			}
-		}
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+            } catch (\DomainException $e) {
+                $this->errors->handle($e);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
 
-		return $this->render('app/users/edit.html.twig', [
-			'user' => $user,
-			'form' => $form->createView(),
-		]);
-	}
+        return $this->render('app/users/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
 
-	/**
-	 * @Route("/{id}/role", name=".role")
-	 * @param User $user
-	 * @param Request $request
-	 * @param Role\Handler $handler
-	 * @return Response
-	 */
-	public function role(User $user, Request $request, Role\Handler $handler): Response
-	{
-		if ($user->getId()->getValue() === $this->getUser()->getId()) {
-			$this->addFlash('error', 'Unable to change role for yourself.');
-			return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-		}
+    /**
+     * @Route("/{id}/role", name=".role")
+     * @param User $user
+     * @param Request $request
+     * @param Role\Handler $handler
+     * @return Response
+     */
+    public function role(User $user, Request $request, Role\Handler $handler): Response
+    {
+        if ($user->getId()->getValue() === $this->getUser()->getId()) {
+            $this->addFlash('error', 'Unable to change role for yourself.');
+            return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+        }
 
-		$command = Role\Command::fromUser($user);
+        $command = Role\Command::fromUser($user);
 
-		$form = $this->createForm(Role\Form::class, $command);
-		$form->handleRequest($request);
+        $form = $this->createForm(Role\Form::class, $command);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid()) {
-			try {
-				$handler->handle($command);
-				return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-			} catch (\DomainException $e) {
-				$this->logger->warning($e->getMessage(), ['exception' => $e]);
-				$this->addFlash('error', $e->getMessage());
-			}
-		}
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+            } catch (\DomainException $e) {
+                $this->errors->handle($e);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
 
-		return $this->render('app/users/role.html.twig', [
-			'user' => $user,
-			'form' => $form->createView(),
-		]);
-	}
+        return $this->render('app/users/role.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
 
-	/**
-	 * @Route("/{id}/confirm", name=".confirm", methods={"POST"})
-	 * @param User $user
-	 * @param Request $request
-	 * @param Confirm\Manual\Handler $handler
-	 * @return Response
-	 */
-	public function confirm(User $user, Request $request, Confirm\Manual\Handler $handler): Response
-	{
-		if (!$this->isCsrfTokenValid('confirm', $request->request->get('token'))) {
-			return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-		}
+    /**
+     * @Route("/{id}/confirm", name=".confirm", methods={"POST"})
+     * @param User $user
+     * @param Request $request
+     * @param Confirm\Manual\Handler $handler
+     * @return Response
+     */
+    public function confirm(User $user, Request $request, Confirm\Manual\Handler $handler): Response
+    {
+        if (!$this->isCsrfTokenValid('confirm', $request->request->get('token'))) {
+            return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+        }
 
-		$command = new Confirm\Manual\Command($user->getId()->getValue());
+        $command = new Confirm\Manual\Command($user->getId()->getValue());
 
-		try {
-			$handler->handle($command);
-		} catch (\DomainException $e) {
-			$this->logger->warning($e->getMessage(), ['exception' => $e]);
-			$this->addFlash('error', $e->getMessage());
-		}
+        try {
+            $handler->handle($command);
+        } catch (\DomainException $e) {
+            $this->errors->handle($e);
+            $this->addFlash('error', $e->getMessage());
+        }
 
-		return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-	}
+        return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+    }
 
-	/**
-	 * @Route("/{id}/activate", name=".activate", methods={"POST"})
-	 * @param User $user
-	 * @param Request $request
-	 * @param Activate\Handler $handler
-	 * @return Response
-	 */
-	public function activate(User $user, Request $request, Activate\Handler $handler): Response
-	{
-		if (!$this->isCsrfTokenValid('activate', $request->request->get('token'))) {
-			return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-		}
+    /**
+     * @Route("/{id}/activate", name=".activate", methods={"POST"})
+     * @param User $user
+     * @param Request $request
+     * @param Activate\Handler $handler
+     * @return Response
+     */
+    public function activate(User $user, Request $request, Activate\Handler $handler): Response
+    {
+        if (!$this->isCsrfTokenValid('activate', $request->request->get('token'))) {
+            return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+        }
 
-		$command = new Activate\Command($user->getId()->getValue());
+        $command = new Activate\Command($user->getId()->getValue());
 
-		try {
-			$handler->handle($command);
-		} catch (\DomainException $e) {
-			$this->logger->warning($e->getMessage(), ['exception' => $e]);
-			$this->addFlash('error', $e->getMessage());
-		}
+        try {
+            $handler->handle($command);
+        } catch (\DomainException $e) {
+            $this->errors->handle($e);
+            $this->addFlash('error', $e->getMessage());
+        }
 
-		return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-	}
+        return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+    }
 
-	/**
-	 * @Route("/{id}/block", name=".block", methods={"POST"})
-	 * @param User $user
-	 * @param Request $request
-	 * @param Block\Handler $handler
-	 * @return Response
-	 */
-	public function block(User $user, Request $request, Block\Handler $handler): Response
-	{
-		if (!$this->isCsrfTokenValid('block', $request->request->get('token'))) {
-			return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-		}
+    /**
+     * @Route("/{id}/block", name=".block", methods={"POST"})
+     * @param User $user
+     * @param Request $request
+     * @param Block\Handler $handler
+     * @return Response
+     */
+    public function block(User $user, Request $request, Block\Handler $handler): Response
+    {
+        if (!$this->isCsrfTokenValid('block', $request->request->get('token'))) {
+            return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+        }
 
-		if ($user->getId()->getValue() === $this->getUser()->getId()) {
-			$this->addFlash('error', 'Unable to block yourself.');
-			return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-		}
+        if ($user->getId()->getValue() === $this->getUser()->getId()) {
+            $this->addFlash('error', 'Unable to block yourself.');
+            return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+        }
 
-		$command = new Block\Command($user->getId()->getValue());
+        $command = new Block\Command($user->getId()->getValue());
 
-		try {
-			$handler->handle($command);
-		} catch (\DomainException $e) {
-			$this->logger->warning($e->getMessage(), ['exception' => $e]);
-			$this->addFlash('error', $e->getMessage());
-		}
+        try {
+            $handler->handle($command);
+        } catch (\DomainException $e) {
+            $this->errors->handle($e);
+            $this->addFlash('error', $e->getMessage());
+        }
 
-		return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
-	}
+        return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+    }
 
-	/**
-	 * @Route("/{id}", name=".show")
-	 * @param User $user
-	 * @param MemberFetcher $members
-	 * @return Response
-	 */
-	public function show(User $user, MemberFetcher $members): Response
-	{
-		$member = $members->find($user->getId()->getValue());
+    /**
+     * @Route("/{id}", name=".show")
+     * @param User $user
+     * @param MemberFetcher $members
+     * @return Response
+     */
+    public function show(User $user, MemberFetcher $members): Response
+    {
+        $member = $members->find($user->getId()->getValue());
 
-		return $this->render('app/users/show.html.twig', compact('user', 'member'));
-	}
+        return $this->render('app/users/show.html.twig', compact('user', 'member'));
+    }
 }
-
-
