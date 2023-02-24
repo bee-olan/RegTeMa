0cf6948558dc28690495c5d3b3701766c38a99e1
olan <bee.olan@mail.ru>
2023-02-25 00:57:26 +0300

    вернулась к 8-2 uchastie + twig+++ добавления в группу

--- manager/src/Model/Adminka/UseCase/Uchasties/Group/Remove/Handler.php
+++ manager/src/Model/Adminka/UseCase/Uchasties/Group/Remove/Handler.php
@@ -4,20 +4,25 @@ declare(strict_types=1);
 
 namespace App\Model\Adminka\UseCase\Uchasties\Group\Remove;
 
+use App\Model\Adminka\Entity\Uchasties\Uchastie\UchastieRepository;
 use App\Model\Flusher;
 use App\Model\Adminka\Entity\Uchasties\Group\GroupRepository;
 use App\Model\Adminka\Entity\Uchasties\Group\Id;
+use App\Model\Work\Entity\Members\Member\MemberRepository;
 
 
 class Handler
 {
     private $groups;
+    private $uchasties;
     private $flusher;
 
-    public function __construct(GroupRepository $groups,                               
+    public function __construct(GroupRepository $groups,
+                                UchastieRepository $uchasties,
                                 Flusher $flusher)
     {
         $this->groups = $groups;
+        $this->uchasties = $uchasties;
         $this->flusher = $flusher;
     }
 
@@ -25,9 +30,9 @@ class Handler
     {
         $group = $this->groups->get(new Id($command->id));
 
-//         if ($this->Uchasties->hasByGroup($group->getId())) {
-//             throw new \DomainException('Группа не пуста.');
-//         }
+         if ($this->uchasties->hasByGroup($group->getId())) {
+             throw new \DomainException('Группа не пуста.');
+         }
 
 
         $this->groups->remove($group);
