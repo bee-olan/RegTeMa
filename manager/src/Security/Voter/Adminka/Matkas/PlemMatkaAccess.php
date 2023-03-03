4de722a414c73c894bf934a64c62a47d4975660d
olan <bee.olan@mail.ru>
2023-03-03 20:29:03 +0300

    вернулась к 9-2  связь  PlemMatkaAccess

---
+++ manager/src/Security/Voter/Adminka/Matkas/PlemMatkaAccess.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Voter\Adminka\Matkas;
+
+use App\Model\Adminka\Entity\Matkas\PlemMatka\PlemMatka;
+use App\Model\Adminka\Entity\Matkas\Role\Permission;
+
+use App\Model\Adminka\Entity\Uchasties\Uchastie\Id;
+use App\Security\UserIdentity;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class  extends Voter
+{
+
+    public const VIEW = 'view';
+    public const MANAGE_UCHASTIES = 'manage_uchasties';
+    public const EDIT = 'edit';
+
+    private $security;
+
+    public function __construct(AuthorizationCheckerInterface $security)
+    {
+        $this->security = $security;
+    }
+
+    protected function supports($attribute, $subject): bool
+    {
+        return in_array($attribute, [self::VIEW, self::MANAGE_UCHASTIES, self::EDIT], true) && $subject instanceof PlemMatka;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        // достаем текущего пользователя
+        $user = $token->getUser();
+        if (!$user instanceof UserIdentity) {
+            return false;
+        }
+// instanceof - проверяет является ли текущий объект экземпляром указанного класса
+        if (!$subject instanceof PlemMatka) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                return
+                    $this->security->isGranted('ROLE_ADMINKA_MANAGE_PLEMMATKAS') ||
+                    $subject->hasUchastie(new Id($user->getId()));
+                break;
+            case self::EDIT:
+                return $this->security->isGranted('ROLE_ADMINKA_MANAGE_PLEMMATKAS');
+                break;
+            case self::MANAGE_UCHASTIES:
+                return
+                    $this->security->isGranted('ROLE_ADMINKA_MANAGE_PLEMMATKAS') ||
+                    $subject->isUchastieGranted(new Id($user->getId()), Permission::MANAGE_PLEMMATKA_UCHASTIES);
+                break;
+        }
+
+        return false;
+    }
+}
