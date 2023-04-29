f10d1aa0c57aa72cd9c8cccdda5f6099b0271bab
bee-olan <bee.olan@mail.ru>
2023-04-29 10:49:24 +0300

    проверяю все+++

---
+++ manager/src/Controller/Proekt/Pasekas/Uchasties/GroupasController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Proekt\Pasekas\Uchasties;
+
+
+use App\Model\Adminka\Entity\Uchasties\Group\Group;
+use App\ReadModel\Adminka\Uchasties\GroupFetcher;
+use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/proekt/pasekas/uchasties/groupas", name="proekt.pasekas.uchasties.groupas")
+ */
+class GroupasController extends AbstractController
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @Route("", name="")
+     * @param GroupFetcher $fetcher
+     * @return Response
+     */
+    public function index(GroupFetcher $fetcher): Response
+    {
+        $groups = $fetcher->all();
+//dd($groups);
+        return $this->render('proekt/pasekas/uchasties/groupas/index.html.twig', compact('groups'));
+    }
+
+}
